b2f55014ac31156b36a07194cce5f0d27635c4f9
Dominic Colson <dominic.colson@hotmail.co.uk>
2019-06-14 20:55:39 +0100

    commit

--- src/cards/cardUI.js
+++ src/cards/cardUI.js
@@ -1,11 +1,12 @@
 import React from "react"
 import img1 from "../assets/deep-blue.jpg"
+import './card-style.css'
 
 const Card = props => {
 return(
     <div className="card text-center">
         <div className="overflow">
-            <img src={img1} alt="image 1"/>
+            <img src={img1} alt="image 1" className="card-img-top"/>
         </div>
         <div className="card-body text-dark">
         <h4 className="card-title">Card Title</h4>
